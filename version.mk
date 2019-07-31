b2d488909232db4d3aba9ebf044a98bc50f3bff7
Mark Yen <mark.yen@suse.com>
2019-07-31 15:41:02 -0700

    version.mk: support concourse github resource
    There's a new telia-oss/github-pr-resource concourse resource that uses
    new GitHub APIs to reduce resource usage, and bundles setting PR status.
    But that doesn't give us the normal version information.

--- version.mk
+++ version.mk
@@ -1,7 +1,18 @@
-GIT_DESCRIBE ?= $(shell git describe --tags --long)
 
-GIT_TAG ?= $(shell echo $(GIT_DESCRIBE) | awk -F - '{ print $$1 }' )
-# GIT_COMMITS will be 0 if we're on the tag
-GIT_COMMITS ?= $(shell echo $(GIT_DESCRIBE) | awk -F - '{ print $$2 }' )
-GIT_SHA ?= $(shell echo $(GIT_DESCRIBE) | awk -F - '{ print $$3 }' )
-APP_VERSION ?= $(GIT_TAG)+$(GIT_COMMITS).$(GIT_SHA)
+# Support concourse telia-oss/github-pr-resource
+ifeq (,$(file <.git/resource/pr))
+
+    GIT_DESCRIBE ?= $(shell git describe --tags --long)
+
+    GIT_TAG ?= $(shell echo $(GIT_DESCRIBE) | awk -F - '{ print $$1 }' )
+    # GIT_COMMITS will be 0 if we're on the tag
+    GIT_COMMITS ?= $(shell echo $(GIT_DESCRIBE) | awk -F - '{ print $$2 }' )
+    GIT_SHA ?= $(shell echo $(GIT_DESCRIBE) | awk -F - '{ print $$3 }' )
+    APP_VERSION ?= $(GIT_TAG)+$(GIT_COMMITS).$(GIT_SHA)
+
+else
+
+    # For PRs, the app version will be "0+PR-#{number}.${head-sha}"
+    APP_VERSION ?= 0+PR-$(file <.git/resource/pr).$(file <.git/resource/head_sha)
+
+endif
